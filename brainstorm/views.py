788cf87f84d76ccf0e8ef5911a45e2cb5ede0592
Dan Drinkard <dan.drinkard@gmail.com>
2013-02-27 22:46:40 -0500

    pass subsite obj, in extra_context, not name

--- brainstorm/views.py
+++ brainstorm/views.py
@@ -15,7 +15,7 @@ def idea_list(request, slug, ordering='-total_upvotes'):
     if hasattr(qs, '_gatekeeper'):
         qs = qs.approved()
     return list_detail.object_list(request, queryset=qs,
-        extra_context={'ordering': ordering, 'subsite': slug}, paginate_by=subsite.ideas_per_page,
+        extra_context={'ordering': ordering, 'subsite': subsite}, paginate_by=subsite.ideas_per_page,
         template_object_name='idea')
 
 
